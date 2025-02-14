53f02ee786c6cc8127847f60c95afeafa49c6ee6
Antônio Guimarães <acgfbr@gmail.com>
2025-02-14 12:17:12 -0300

    vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
-import { FloatingSkulls } from './components/FloatingSkulls';
-import { RandomMessage } from './components/RandomMessage';
-import { AnimatedBackground } from './components/AnimatedBackground';
-import { AnimatedTitle } from './components/AnimatedTitle';
-import { AnimatedContainer } from './components/AnimatedContainer';
-import { Counter } from './components/Counter';
-import { DumbledoreQuote } from './components/DumbledoreQuote';
+import { FloatingSkulls } from "./components/FloatingSkulls";
+import { RandomMessage } from "./components/RandomMessage";
+import { AnimatedBackground } from "./components/AnimatedBackground";
+import { AnimatedTitle } from "./components/AnimatedTitle";
+import { AnimatedContainer } from "./components/AnimatedContainer";
+import { Counter } from "./components/Counter";
+import { DumbledoreQuote } from "./components/DumbledoreQuote";
+import { Analytics } from "@vercel/analytics/react";
 
-export const runtime = 'edge';
-export const preferredRegion = 'auto';
-export const dynamic = 'force-dynamic';
+export const runtime = "edge";
+export const preferredRegion = "auto";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   return (
     <div className="min-h-screen bg-black text-green-500 flex flex-col items-center justify-center relative overflow-hidden">
+      <Analytics />
       <FloatingSkulls />
       <AnimatedBackground />
 
       <div className="text-center z-20 mt-[15vh] md:mt-[20vh] relative">
         <AnimatedTitle />
-        
+
         <AnimatedContainer>
           <Counter />
         </AnimatedContainer>
